92805b042675cce442c69e549bcd34faa43f4c59
Zyrus <zyrusdiaz18@gmail.com>
2019-10-07 18:43:56 +0800

    fcm

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Hangover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPA = new System.Windows.Forms.Label();
+            this.lblPB = new System.Windows.Forms.Label();
+            this.lblPC = new System.Windows.Forms.Label();
+            this.txtPA_AT = new System.Windows.Forms.TextBox();
+            this.txtPB_AT = new System.Windows.Forms.TextBox();
+            this.txtPC_AT = new System.Windows.Forms.TextBox();
+            this.txtPA_BT = new System.Windows.Forms.TextBox();
+            this.txtPB_BT = new System.Windows.Forms.TextBox();
+            this.txtPC_BT = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblLowest = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPA
+            // 
+            this.lblPA.AutoSize = true;
+            this.lblPA.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPA.Location = new System.Drawing.Point(70, 106);
+            this.lblPA.Name = "lblPA";
+            this.lblPA.Size = new System.Drawing.Size(33, 25);
+            this.lblPA.TabIndex = 0;
+            this.lblPA.Text = "PA";
+            // 
+            // lblPB
+            // 
+            this.lblPB.AutoSize = true;
+            this.lblPB.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPB.Location = new System.Drawing.Point(70, 152);
+            this.lblPB.Name = "lblPB";
+            this.lblPB.Size = new System.Drawing.Size(33, 25);
+            this.lblPB.TabIndex = 1;
+            this.lblPB.Text = "PB";
+            // 
+            // lblPC
+            // 
+            this.lblPC.AutoSize = true;
+            this.lblPC.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPC.Location = new System.Drawing.Point(70, 201);
+            this.lblPC.Name = "lblPC";
+            this.lblPC.Size = new System.Drawing.Size(34, 25);
+            this.lblPC.TabIndex = 2;
+            this.lblPC.Text = "PC";
+            // 
+            // txtPA_AT
+            // 
+            this.txtPA_AT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPA_AT.Location = new System.Drawing.Point(119, 106);
+            this.txtPA_AT.Name = "txtPA_AT";
+            this.txtPA_AT.Size = new System.Drawing.Size(126, 32);
+            this.txtPA_AT.TabIndex = 3;
+            // 
+            // txtPB_AT
+            // 
+            this.txtPB_AT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPB_AT.Location = new System.Drawing.Point(119, 152);
+            this.txtPB_AT.Name = "txtPB_AT";
+            this.txtPB_AT.Size = new System.Drawing.Size(126, 32);
+            this.txtPB_AT.TabIndex = 4;
+            // 
+            // txtPC_AT
+            // 
+            this.txtPC_AT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPC_AT.Location = new System.Drawing.Point(119, 198);
+            this.txtPC_AT.Name = "txtPC_AT";
+            this.txtPC_AT.Size = new System.Drawing.Size(126, 32);
+            this.txtPC_AT.TabIndex = 5;
+            // 
+            // txtPA_BT
+            // 
+            this.txtPA_BT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPA_BT.Location = new System.Drawing.Point(269, 106);
+            this.txtPA_BT.Name = "txtPA_BT";
+            this.txtPA_BT.Size = new System.Drawing.Size(126, 32);
+            this.txtPA_BT.TabIndex = 6;
+            // 
+            // txtPB_BT
+            // 
+            this.txtPB_BT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPB_BT.Location = new System.Drawing.Point(269, 152);
+            this.txtPB_BT.Name = "txtPB_BT";
+            this.txtPB_BT.Size = new System.Drawing.Size(126, 32);
+            this.txtPB_BT.TabIndex = 7;
+            // 
+            // txtPC_BT
+            // 
+            this.txtPC_BT.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPC_BT.Location = new System.Drawing.Point(269, 198);
+            this.txtPC_BT.Name = "txtPC_BT";
+            this.txtPC_BT.Size = new System.Drawing.Size(126, 32);
+            this.txtPC_BT.TabIndex = 8;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.Location = new System.Drawing.Point(153, 236);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(202, 54);
+            this.btnCompute.TabIndex = 9;
+            this.btnCompute.Text = "COMPUTE";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(128, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Arrival Time";
+            // 
+            // lblLowest
+            // 
+            this.lblLowest.AutoSize = true;
+            this.lblLowest.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowest.Location = new System.Drawing.Point(129, 372);
+            this.lblLowest.Name = "lblLowest";
+            this.lblLowest.Size = new System.Drawing.Size(16, 19);
+            this.lblLowest.TabIndex = 11;
+            this.lblLowest.Text = "0";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(135, 337);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(75, 32);
+            this.txt1.TabIndex = 12;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(216, 337);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(75, 32);
+            this.txt2.TabIndex = 13;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(297, 337);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(75, 32);
+            this.txt3.TabIndex = 14;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(205, 372);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(16, 19);
+            this.lbl1.TabIndex = 16;
+            this.lbl1.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(285, 372);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(16, 19);
+            this.lbl2.TabIndex = 17;
+            this.lbl2.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(366, 372);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(16, 19);
+            this.lbl3.TabIndex = 18;
+            this.lbl3.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semilight", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(280, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 25);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Burst Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(509, 485);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblLowest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtPC_BT);
+            this.Controls.Add(this.txtPB_BT);
+            this.Controls.Add(this.txtPA_BT);
+            this.Controls.Add(this.txtPC_AT);
+            this.Controls.Add(this.txtPB_AT);
+            this.Controls.Add(this.txtPA_AT);
+            this.Controls.Add(this.lblPC);
+            this.Controls.Add(this.lblPB);
+            this.Controls.Add(this.lblPA);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPA;
+        private System.Windows.Forms.Label lblPB;
+        private System.Windows.Forms.Label lblPC;
+        private System.Windows.Forms.TextBox txtPA_AT;
+        private System.Windows.Forms.TextBox txtPB_AT;
+        private System.Windows.Forms.TextBox txtPC_AT;
+        private System.Windows.Forms.TextBox txtPA_BT;
+        private System.Windows.Forms.TextBox txtPB_BT;
+        private System.Windows.Forms.TextBox txtPC_BT;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblLowest;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangover
+{
+    public partial class Form1 : Form
+    {
+        List<Label> labels;
+        List<TextBox> textboxes;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //Control[] controls = { lblPA,lblPB,};
+            labels = new List<Label> {lbl1, lbl2, lbl3 };
+            textboxes = new List<TextBox>{ txt1, txt2, txt3 };
+        }
+        
+        List<int> nums = new List<int>();
+        String process = "";
+        int tempAT=0, tempBT=0,currAT= 0,currBT=0;
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            int[] pa = { int.Parse(txtPA_AT.Text), int.Parse(txtPA_BT.Text) };
+            int[] pb = { int.Parse(txtPB_AT.Text), int.Parse(txtPB_BT.Text) };
+            int[] pc = { int.Parse(txtPC_AT.Text), int.Parse(txtPC_BT.Text) };
+            insertToList(pa);
+            insertToList(pb);
+            insertToList(pc);
+            nums.Sort();
+            lblLowest.Text = nums[0].ToString();
+            compareArrays(pa, pb, pc);
+        }
+
+        public void compareArrays(int[]pa,int[]pb,int[]pc)
+        {
+            List<String> processes = new List<String> { "PA", "PB", "PC" };
+            while (processes.Count>0){
+                //Arrival Time
+                if (processes.Contains("PA")) {
+                    if (pa[0] < pb[0] && pa[0] < pc[0])
+                    {//PA yung tama
+                        MessageBox.Show("PA");
+                        process = processes[0];
+                        processes.RemoveAt(0);
+
+                        currAT = pa[0];
+                        currBT = pa[1] + tempBT;
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if(labels[i].Text != "")
+                            {
+                                labels[i].Text = currBT.ToString();
+                            }
+                        }
+                        tempBT = currBT;
+                        //TextBox
+                        for (int i = 0; i < 2; i++) {
+                            if (textboxes[i].Text != "")
+                            {
+                                textboxes[i].Text =process;
+                            }
+                        }
+                        tempAT = currAT;
+                    }
+                }else if (processes.Contains("PB"))
+                {
+                    if (pb[0] < pa[0] && pb[0] < pc[0])
+                    {//PB tama
+                        MessageBox.Show("PB");
+                        process = processes[1];
+                        processes.RemoveAt(1);
+                        currAT = pb[0];
+                        currBT = pb[1] + tempBT;
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (labels[i].Text != "")
+                            {
+                                labels[i].Text = currBT.ToString();
+                            }
+                        }
+                        tempBT = currBT;
+                        txt1.Text = processes[1];
+                        tempAT = currAT;
+                    }
+                }else if (processes.Contains("PC"))
+                {
+                    MessageBox.Show("PC");
+                    if (pc[0] < pa[0] && pc[0] < pb[0])
+                    {//PC tama
+                        process = processes[2];
+                        processes.RemoveAt(2);
+                        currAT = pc[0];
+                        currBT = pc[1] + tempBT;
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (labels[i].Text != "")
+                            {
+                                labels[i].Text = currBT.ToString();
+                            }
+                        }
+                        tempBT = currBT;
+                        txt3.Text = processes[2];
+                        tempAT = currAT;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Paalam kapatid");
+                }
+            }
+        }
+
+        public void insertToList(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                nums.Add(x[i]);
+                Console.WriteLine(x[i] + "Added to list");
+            }
+        }
+
+        
+    }
+}
